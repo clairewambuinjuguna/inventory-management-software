3d71cbf6fe4583019cc25b8fc3116bce202595f9
clairewambuinjuguna <clairenjugunaw@gmail.com>
2024-06-12 16:12:27 +0300

    edited the sidebar and added new links

--- app/(back-office)/components/dashboard/Sidebar.jsx
+++ app/(back-office)/components/dashboard/Sidebar.jsx
@@ -22,11 +22,23 @@ export default function Sidebar() {
   const InventoryLinks = [
     {
       title: "Items",
-      href: "/dashboard/inventory",
+      href: "/dashboard/inventory/items/new",
     },
     {
-      title: "Item Groups",
-      href: "/dashboard/inventory",
+      title: "Categories",
+      href: "/dashboard/inventory/categories/new",
+    },
+    {
+      title: "Brands",
+      href: "/dashboard/inventory/brands/new",
+    },
+    {
+      title: "Units",
+      href: "/dashboard/inventory/units/new",
+    },
+    {
+      title: "Warehouse",
+      href: "/dashboard/inventory/warehouse/new",
     },
     {
       title: "Inventory Adjustments",
--- app/(back-office)/components/dashboard/SidebarDropdownLink.jsx
+++ app/(back-office)/components/dashboard/SidebarDropdownLink.jsx
@@ -5,16 +5,19 @@ import {
   CollapsibleContent,
   CollapsibleTrigger,
 } from "@/components/ui/collapsible";
-import { BaggageClaim } from "lucide-react";
+import { BaggageClaim, ChevronRight } from "lucide-react";
 import CollapsibleLink from "./CollapsibleLink";
 
 export default function SidebarDropdownLink({ title, items, icon: Icon }) {
   return (
     <div>
       <Collapsible>
-        <CollapsibleTrigger className="flex items-center space-x-2 p-2">
-          <Icon className="w-4 h-4" />
-          <span>{title}</span>
+        <CollapsibleTrigger className="flex justify-between items-center  w-full">
+          <div className="flex items-center space-x-2 p-2">
+            <Icon className="w-3 h-3" />
+            <span>{title}</span>
+          </div>
+          <ChevronRight className="flex justify-center" />
         </CollapsibleTrigger>
         <CollapsibleContent>
           {items.map((item, i) => (
