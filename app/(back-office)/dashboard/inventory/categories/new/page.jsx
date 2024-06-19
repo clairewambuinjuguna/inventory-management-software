1de925a043763ceeb2edbe58ba221832a0f6a4db
clairewambuinjuguna <clairenjugunaw@gmail.com>
2024-06-19 15:49:32 +0300

    created text area component

--- app/(back-office)/dashboard/inventory/categories/new/page.jsx
+++ app/(back-office)/dashboard/inventory/categories/new/page.jsx
@@ -5,16 +5,22 @@ import { X } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 import { useForm } from "react-hook-form";
+import { useState } from "react";
 
+import Submitbutton from "@/app/(back-office)/components/Forminputs/Submitbutton";
+import TextareaInput from "@/app/(back-office)/components/Forminputs/Textareainput";
 export default function NewCategory() {
   const {
     register,
     handleSubmit,
-    watch,
+    reset,
     formState: { errors },
   } = useForm();
+  const [loading, setLoading] = useState(false);
 
   function onSubmit(data) {
+    reset();
+    // setLoading(true);
     console.log(data);
   }
 
@@ -34,16 +40,16 @@ export default function NewCategory() {
             register={register}
             errors={errors}
           />
+          <div className="sm:col-span-2">
+            <TextareaInput
+              label="Category Description"
+              name="Category"
+              register={register}
+              errors={errors}
+            />
+          </div>
         </div>
-        <div className="py-4 mt-6 sm:col-span-1">
-          <button
-            type="submit"
-            className=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Save
-          </button>
-          <div className="text-sm font-medium text-gray-500 dark:text-gray-300"></div>
-        </div>
+        <Submitbutton isLoading={loading} title="Category" />
       </form>
     </div>
   );
