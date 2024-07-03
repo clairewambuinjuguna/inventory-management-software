3707dc6c20bbf191c99ecad0def0d9f989a848ce
clairewambuinjuguna <clairenjugunaw@gmail.com>
2024-07-03 13:26:55 +0300

    Connected prisma to the category API

--- app/api/categories/route.js
+++ app/api/categories/route.js
@@ -1,9 +1,11 @@
 import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+const db = new PrismaClient();
 export async function POST(request) {
   try {
     const { title, description } = await request.json();
 
-    const category = { title, description };
+    const category = await db.category.create({ data: { title, description } }); //access the modal ie category, then an access method-(create) which takes in an object of data
     console.log(category);
     return NextResponse.json(category);
   } catch (error) {
